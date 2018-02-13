7f55a09a5d07e781049e4c8e0c04fc69ffa38e04
developer <developer>
2018-02-12 21:57:37 -0600

    Methodoverloading-Plymorphism

---
+++ src/main/java/Polyloading.java
@@ -0,0 +1,26 @@
+class AreaOfShape
+{
+    public int  area(int l,int b)
+    {
+        return l*b;
+    }
+    public  float area(float r){
+        return r*r;
+        //return Math.PI(r*r);
+    }
+    public double area(int b , double h){
+        return 0.5*b*h;
+    }
+}
+
+//Method Overloading---same method name with different parameters
+
+public class Polyloading {
+    public static void main(String[] args)
+    {
+        AreaOfShape ar = new AreaOfShape();
+        System.out.println("Area of Circle" + ar.area(2.2f));
+        System.out.println("Are of a Rectangle" + ar.area(4,5));
+        System.out.println("Area of Triangle" + ar.area(3,6.6));
+    }
+}
